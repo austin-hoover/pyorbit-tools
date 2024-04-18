367142a77616e5e9f17d15b99288603c8087744b
austin-hoover <hooveram@ornl.gov>
2024-04-18 12:54:17 -0400

    Change name to pyorbit-tools

--- test/test_bunch.py
+++ test/test_bunch.py
@@ -1,8 +1,10 @@
 import numpy as np
+
 import orbit.core
 import orbit.lattice
 from orbit.core.bunch import Bunch
-import orbitsim
+
+import orbit_tools as pot
 
 
 def make_gaussian_bunch(size: int = 128) -> Bunch:
@@ -15,12 +17,12 @@ def make_gaussian_bunch(size: int = 128) -> Bunch:
 
 def test_set_current():
     bunch = make_gaussian_bunch()
-    orbitsim.bunch.set_current(bunch, current=0.025, frequency=402.5e+06)
+    pot.bunch.set_current(bunch, current=0.025, frequency=402.5e+06)
 
 
 def test_get_coords():
     bunch = make_gaussian_bunch()
-    coords = orbitsim.bunch.get_coords(bunch)
+    coords = pot.bunch.get_coords(bunch)
     for i in range(coords.shape[0]):
         assert bunch.x(i) == coords[i, 0]
         assert bunch.y(i) == coords[i, 2]
@@ -33,8 +35,8 @@ def test_get_coords():
 def test_set_coords():
     coords = np.random.normal(size=(128, 6))
     bunch = Bunch()
-    bunch = orbitsim.bunch.set_coords(bunch, coords)
-    assert np.all(coords == orbitsim.bunch.get_coords(bunch))
+    bunch = pot.bunch.set_coords(bunch, coords)
+    assert np.all(coords == pot.bunch.get_coords(bunch))
 
 
 def test_decorrelate_x_y_z():
@@ -48,10 +50,10 @@ def test_decorrelate_x_y_z():
     cov = np.cov(coords.T)
 
     bunch = Bunch()
-    bunch = orbitsim.bunch.set_coords(bunch, coords)
-    bunch = orbitsim.bunch.decorrelate_x_y_z(bunch)
+    bunch = pot.bunch.set_coords(bunch, coords)
+    bunch = pot.bunch.decorrelate_x_y_z(bunch)
 
-    coords_out = orbitsim.bunch.get_coords(bunch)
+    coords_out = pot.bunch.get_coords(bunch)
     cov_out = np.cov(coords_out.T)
     for (i, j) in indices:
         assert np.abs(cov_out[i, j]) < np.abs(cov[i, j])
@@ -68,10 +70,10 @@ def test_decorrelate_xy_z():
     cov = np.cov(coords.T)
 
     bunch = Bunch()
-    bunch = orbitsim.bunch.set_coords(bunch, coords)
-    bunch = orbitsim.bunch.decorrelate_xy_z(bunch)
+    bunch = pot.bunch.set_coords(bunch, coords)
+    bunch = pot.bunch.decorrelate_xy_z(bunch)
 
-    coords_out = orbitsim.bunch.get_coords(bunch)
+    coords_out = pot.bunch.get_coords(bunch)
     cov_out = np.cov(coords_out.T)
     for (i, j) in indices:
         assert np.abs(cov_out[i, j]) < np.abs(cov[i, j])
@@ -81,39 +83,39 @@ def test_downsample():
     size = 1000
     new_size = 100
     bunch = make_gaussian_bunch()
-    bunch = orbitsim.bunch.downsample(bunch, new_size, conserve_intensity=True)
+    bunch = pot.bunch.downsample(bunch, new_size, conserve_intensity=True)
     new_bunch_size_global = bunch.getSizeGlobal()
     assert new_bunch_size_global == new_size
 
 
 def test_revese_bunch():
     bunch = make_gaussian_bunch()
-    bunch = orbitsim.bunch.reverse(bunch)
+    bunch = pot.bunch.reverse(bunch)
 
 
 def test_get_centroid():
     bunch = make_gaussian_bunch()
-    centroid = orbitsim.bunch.get_centroid(bunch)
+    centroid = pot.bunch.get_centroid(bunch)
 
 
 def test_shift_centroid():
     bunch = make_gaussian_bunch()
-    bunch = orbitsim.bunch.shift_centroid(bunch, np.ones(6))
+    bunch = pot.bunch.shift_centroid(bunch, np.ones(6))
 
 
 def test_set_centroid():
     bunch = make_gaussian_bunch()
-    bunch = orbitsim.bunch.set_centroid(bunch, np.ones(6))
+    bunch = pot.bunch.set_centroid(bunch, np.ones(6))
 
 
 def test_get_mean_and_cov():
     bunch = make_gaussian_bunch()
-    mean, cov = orbitsim.bunch.get_mean_and_cov(bunch)
+    mean, cov = pot.bunch.get_mean_and_cov(bunch)
 
 
 def test_load_bunch():
     filename = None
-    orbitsim.bunch.load(filename=None)
+    pot.bunch.load(filename=None)
 
 
 def test_generate():
@@ -121,10 +123,10 @@ def test_generate():
         return np.zeros(6)
 
     bunch = Bunch()
-    bunch = orbitsim.bunch.generate(func, size=128, bunch=bunch)
+    bunch = pot.bunch.generate(func, size=128, bunch=bunch)
     return bunch
 
 
 def test_get_twiss_containers():
     bunch = make_gaussian_bunch()
-    twiss_x, twiss_y, twiss_z = orbitsim.bunch.get_twiss_containers(bunch)
+    twiss_x, twiss_y, twiss_z = pot.bunch.get_twiss_containers(bunch)
