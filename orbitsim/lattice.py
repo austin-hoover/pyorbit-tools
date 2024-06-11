98ce79e162e80674c6c6812eb66b4ebecc18abda
austin-hoover <hooveram@ornl.gov>
2024-06-11 14:55:47 -0400

    Add get_node_name_and_index utility function

--- orbitsim/lattice.py
+++ orbitsim/lattice.py
@@ -129,3 +129,37 @@ def get_node_for_name_any_case(lattice: AccLattice, name: str) -> AccNode:
         else:            
             raise ValueError(f"node {name} not found")
     return lattice.getNodeForName(name)
+
+
+def get_node_name_and_index(
+    lattice: AccLattice, node: AccNode = None, name: str = None, index: int = None,
+) -> tuple[AccNode, str, int]:
+    if node is None:
+        if name is not None:
+            node = lattice.getNodeForName(name)
+        elif index is not None:
+            node = lattice.getNodes()[index]
+        else:
+            raise ValueError("Must provide node, name or index.")
+    name = node.getName()
+    index = lattice.getNodeIndex(node)
+    return (node, name, index)
+
+
+def get_nodes_names_and_indices(
+    lattice: AccLattice,
+    nodes: list[AccNode] = None, 
+    names: list[str] = None,
+    indices: list[int] = None,
+) -> tuple[list[AccNode], list[str], list[int]]:
+    if nodes is None:
+        if names is not None:
+            nodes = [lattice.getNodeForName(name) for name in names]
+        elif indices is not None:
+            nodes = [lattice.getNodes()[index] for index in indices]
+        else:
+            raise ValueError("Must provide nodes, names or indices.")
+
+    names = [node.getName() for node in nodes]
+    indices = [lattice.getNodeIndex(node) for node in nodes]
+    return (nodes, names, indices)
